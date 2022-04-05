baf1c5cf4feb38e6ab53e9ab34e949956442f9b3
Zelie Chowaiki <zeliechowaiki@gmail.com>
2022-04-05 14:04:50 -0400

    commit tree d9eea7965efffae3a780bdbdcac3059d41f951dd

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -22,10 +22,30 @@ class BirdsController < ApplicationController
     end
   end
 
+  def update
+    bird = Bird.find(params[:id])
+    if bird
+      bird.update(bird_params)
+      render json: bird
+    else
+      render json: {error: "Bird not found"}, status: :not_found
+    end
+  end
+
+  def increment_likes
+    bird = Bird.find(params[:id])
+    if bird
+      bird.update(likes: bird.likes + 1)
+      render json: bird
+    else
+      render json: { error: "Bird not found" }, status: :not_found
+    end
+  end
+
   private
 
   def bird_params
-    params.permit(:name, :species)
+    params.permit(:name, :species, :likes)
   end
 
 end
